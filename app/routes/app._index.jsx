9e5ebc676c69c70f1b065c2e2ae18df52f766338
garvit-ai <garrvitb@gmail.com>
2026-09-03 00:02:14 +0530

    updated app.index

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,3 +1,5 @@
 import { redirect } from "react-router";
 
-export const loader = async () => redirect("/app/pincodes");
+export const loader = async () => {
+    return redirect("/app/pincodes");
+};
